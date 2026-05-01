43d0ab47af6786224ad33187bae7eced89c0de99
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-05-01 16:56:23 +0700

    feat(config): add tmux config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,11 @@
 package config
 
 type Config struct {
-	Projects  []string        `mapstructure:"projects" json:"projects"`
+	Projects []string   `mapstructure:"projects" json:"projects"`
+	Tmux     TmuxConfig `mapstructure:"tmux" json:"tmux"`
+}
+
+type TmuxConfig struct {
+	SessionPrefix  string `mapstructure:"session_prefix" json:"session_prefix"`
+	AttachOnCreate bool   `mapstructure:"attach_on_create" json:"attach_on_create"`
 }
