b6f4a2092494632604a91f9f9a19a7457abfcc9f
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-05-03 10:32:25 +0700

    feat: check if tmux installed

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -11,12 +12,21 @@ func rootRun(cmd *cobra.Command, args []string) error {
 	return projectsRun(cmd, args)
 }
 
+func checkTmuxInstalled(cmd *cobra.Command, args []string) {
+	_, err := exec.LookPath("tmuxs")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "tmux is not installed, please install it before using tmux-mgr.")
+		os.Exit(1)
+	}
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "tmux-mgr",
 	Short: "A CLI/TUI to manage tmux sessions and projects",
 	Long: `tmux-mgr is a tmux session manager that automates how you create and attach to tmux sessions.
 It replaces manual commands with a clean terminal interface for faster navigation.`,
-	RunE: rootRun,
+	PersistentPreRun: checkTmuxInstalled,
+	RunE:             rootRun,
 }
 
 func Execute() {
